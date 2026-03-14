81797da81a2199fafd4816c787d79588a70c9626
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-03-14 13:24:08 +0300

    sss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-//import { Component } from '@angular/core';
 import { Component } from '@angular/core';
 import './training.ts';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
+  companyName: string = 'Румтибет';
+
+  constructor() {
+    this.saveLastVisitDate();
+    this.saveVisitCount();
+  }
+
+  saveLastVisitDate(): void {
+    const date = new Date();
+    const dateString = date.toString();
+    localStorage.setItem('date', dateString);
+  }
+
+
+  saveVisitCount(): void {
+  const currentCount = localStorage.getItem('visitCount');
+  
+  let newCount: number;
+  
+  if (currentCount) {
+    newCount = Number(currentCount) + 1;
+  } else {
+    newCount = 1;
+  }
+
+  localStorage.setItem('visitCount', newCount.toString());
+ }
 }
+
