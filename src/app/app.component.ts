d856844c3750e8092d7c6941875800e649727110
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-03-17 14:06:13 +0300

    not fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import './training.ts';
+import { IAdvantage } from './interfaces/IAdvantag.js';
+import { FormsModule } from '@angular/forms';
+import { ILocation } from './interfaces/ILocation.js';
+import { IParticipant } from './interfaces/IParticipant.js';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: []
 })
 export class AppComponent {
 
+
   companyName: string = 'Румтибет';
+  selectedAdvantagId?: number;
+  selectedLocation!: boolean;
+  selectedParticipant!: boolean;
+  selectedDate!: boolean;
+  clicker: number = 0;
+  readonly zero: number = 0;
+  dataText!: string | number;
+  private data!: Date;
+  liveInput!: string | number;
+  isLoading: boolean = false;
+  underline: boolean = false;
+  selectedUnderline!: number;
+
+
+  locations: ILocation[] = [
+    {
+      id: 1,
+      name: 'dagestan'
+    },
+    {
+      id: 2,
+      name: 'moscow'
+    }
+  ]
+
+  participants: IParticipant[] = [
+    {
+      id: 1,
+      quantity: 'участники',
+    },
+    {
+      id: 2,
+      quantity: 'участник 1',
+    },
+    {
+      id: 3,
+      quantity: 'участник 2',
+    }
+  ]
+
+
+  advantages: IAdvantage[] = [
+    {
+      id: 1,
+      title: 'Опытный гид',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    },
+    {
+      id: 2,
+      title: 'Безопасный поход',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    },
+    {
+      id: 3,
+      title: 'Лояльные цены',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+    }
+  ]
+
 
   constructor() {
     this.saveLastVisitDate();
     this.saveVisitCount();
-  }
+    this.plus();
+    this.dataText;
+    this.data;
+    this.clicker = 0;
 
+    setInterval(() => {
+      this.data = new Date();
+      this.dataText = this.data.toString();
+    }, 1000);
+  
+  
+    setInterval(() => {
+      this.isLoading = true;
+    }, 2000);
+  }
   saveLastVisitDate(): void {
-    const date = new Date();
-    const dateString = date.toString();
+    const date: Date = new Date();
+    const dateString: string = date.toString();
     localStorage.setItem('date', dateString);
   }
 
-
   saveVisitCount(): void {
-  const currentCount = localStorage.getItem('visitCount');
+  const currentCount: string | null = localStorage.getItem('visit-count');
   
   let newCount: number;
   
@@ -33,7 +112,27 @@ export class AppComponent {
     newCount = 1;
   }
 
-  localStorage.setItem('visitCount', newCount.toString());
+  localStorage.setItem('visit-count', newCount.toString());
  }
+
+  selectService(advantagId: number): void {
+      this.selectedAdvantagId = advantagId;
+    }
+
+  plus() {
+    this.clicker += 1;
+  }
+
+  minus(): void {
+    if (this.clicker > 0) {
+      this.clicker -= 1;
+    }
+  }
+
+  isValidForm(): boolean {
+    return this.selectedLocation && this.selectedParticipant && this.selectedDate;
+  }
+
+  
 }
 
