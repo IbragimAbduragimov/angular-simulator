8938736253be7066c068a3e31f5dbf2e4d496463
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-04-25 14:08:33 +0300

    fix

--- src/footer/footer.component.ts
+++ src/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IImportant } from '../app/interfaces/IImportant';
+import { IbBusinessServices } from '../app/interfaces/IBusinessServices';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
-}
+   
+    businessServices: IbBusinessServices[] = [
+      {
+        service: 'Прогулки в горы летом'
+      },
+          {
+        service: 'Зимние походы в горы'
+      },
+          {
+        service: 'Посещение мест в горах'
+      },
+          {
+        service: 'Экстремальные виды туризма'
+      },
+          {
+        service: 'Походы в джунглях Амазонии'
+      },
+          {
+        service: 'Поездка в Африку'
+      },
+    ]
+  
+    importants: IImportant[] = [
+      {
+        important: 'Как собрать в долгий поход?'
+      },
+          {
+        important: 'Жизненно важные предметы для похода'
+      },
+          {
+        important: 'Медицинская страховка, гарантии безопасности'
+      },
+          {
+        important: 'Если вы врач - загляните сюда'
+      }
+    ]
+    
+  }
--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,30 +1,22 @@
 import { Component } from '@angular/core';
-import { Widget } from '../app/types/Widget';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
 
-  companyName: string = 'Румтибет'; 
-  currentWidget: string = 'data';
-  clicker!: number;
-  dateText!: string;
-
-  toggleWidget(widget: Widget) {
-    this.currentWidget = widget;
-  } 
-
-  increment(): void {
-    this.clicker + 1;
-  }
-
-  decrement(): void {
-    if (this.clicker > 0) {  
-      this.clicker - 1;
+  navigations = [
+    {
+      routerLink: '',
+      text: 'Главная'
+    },
+    {
+      routerLink: 'users',
+      text: 'Пользователи'
     }
-  }
+  ]
 }
