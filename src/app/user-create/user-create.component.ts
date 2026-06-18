74ee19afb1349d1668f81ee786704532004a5110
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-06-18 17:15:10 +0300

    correct

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -2,12 +2,12 @@ import { Component, EventEmitter, inject, Output, output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { IUser } from '../interfaces/IUser';
 import { UserService } from '../user.service';
-import { BoldHowerDirective } from "../bold.directive";
 import { GradientHowerDirective } from "../gradient-hower.directive";
+import { BoldOnHoverDirective } from '../bold.directive';
 
 @Component({
   selector: 'app-user-create',
-  imports: [ReactiveFormsModule, BoldHowerDirective, GradientHowerDirective],
+  imports: [ReactiveFormsModule, BoldOnHoverDirective, GradientHowerDirective, BoldOnHoverDirective],
   templateUrl: './user-create.component.html',
   styleUrl: './user-create.component.scss',
 })
