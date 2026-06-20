a911b8fb7acf624b0956d00097062b76aaf949e9
IbragimAbduragimov <ibragimabduragimov666@gmail.com>
2026-06-20 11:30:03 +0300

    fix

--- src/app/plural.pipe.ts
+++ src/app/plural.pipe.ts
@@ -8,10 +8,12 @@ export class PrularPipe implements PipeTransform {
   transform(numberOf: number, firstForm: string, secondForm: string, thirdForm: string): string {
 
     const formNumbers: string = numberOf.toString().slice(-1);
+    const exceptions: number[] = [12, 13, 14];
+    const secondFormNumbers: string[] = ['2', '3', '4'];
 
     if (formNumbers === '1' && numberOf !== 11) {
     return `${ numberOf } ${ firstForm }`;
-    } else if(['2', '3', '4'].includes(formNumbers) && ![12, 13, 14].includes(numberOf)) {
+    } else if(secondFormNumbers.includes(formNumbers) && !exceptions.includes(numberOf)) {
     return `${ numberOf } ${ secondForm }`;
     } else {
     return `${ numberOf } ${ thirdForm }`;
